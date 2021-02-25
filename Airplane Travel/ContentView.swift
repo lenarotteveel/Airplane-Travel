65bcaaa526a08d78842c3a5677a411a711301a5c
Lena Rotteveel <lenarotteveel@gmail.com>
2021-02-25 10:18:42 -0600

    Initial Commit

---
+++ Airplane Travel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Airplane Travel
+//
+//  Created by Lena Rotteveel on 2/25/21.
+//  Copyright © 2021 Lena Rotteveel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
